f2c3fe1ac8616b4545844117a0f9390e8c55d662
Maciej Wątroba <maciek.bb@gmail.com>
2013-12-14 15:55:27 +0100

    Arms move quite nicely

--- arm2/MainWindow.xaml.cs
+++ arm2/MainWindow.xaml.cs
@@ -20,6 +20,11 @@ namespace arm2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double TransitionX = 0;
+        private double TransitionY = 0;
+        private double BetweenAngle = 0;
+        private double FirstChange = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,21 +32,62 @@ namespace arm2
 
         private void Base_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            //currentStatus.Content = e.NewValue.ToString();
-            RotateTransform rotate = new RotateTransform((e.NewValue) * 360);
-            rotate.CenterX = 150;
-            rotate.CenterY = 170;
-            armTopViewPolyline.RenderTransform = rotate;
+            moveBase(e.NewValue);
         }
 
         private void FirstArm_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            MoveFirstArm(e.NewValue);
         }
 
         private void SecondArm_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            MoveSecondArm(e.NewValue);
         }
+
+        private void moveBase(double e)
+        {
+            Grid canvas = armTopViewPolyline.Parent as Grid;
+            RotateTransform rotate = new RotateTransform((e - 0.5) * 90);
+            rotate.CenterX = canvas.Width / 2;
+            rotate.CenterY = canvas.Height / 2;
+            armTopViewPolyline.RenderTransform = rotate;
+        }
+        
+        void MoveFirstArm(double e) {
+            Grid canvas = armSideViewPolygon1.Parent as Grid;
+            RotateTransform rotate = new RotateTransform(-(e) * 135);
+            FirstChange = e;
+            rotate.CenterX = canvas.Width / 2;
+            rotate.CenterY = canvas.Height / 2 + 20;
+            armSideViewPolygon1.RenderTransform = rotate;
+
+            TransformGroup allTransforms = new TransformGroup();
+            TransitionX = 60*Math.Cos(-(e)*2.36)-60;
+            TransitionY = 60*Math.Sin(-(e)*2.36);
+           
+            RotateTransform rotate2 = new RotateTransform(-(e-BetweenAngle) * 135);
+            rotate2.CenterX = canvas.Width / 2 + 50 + TransitionX;
+            rotate2.CenterY = canvas.Height / 2 + 20 + TransitionY;
+            allTransforms.Children.Add(new TranslateTransform(TransitionX, TransitionY));
+            allTransforms.Children.Add(rotate2);
+
+            armSideViewPolygon2.RenderTransform = allTransforms;
+        }
+
+        void MoveSecondArm(double e)
+        {
+              TransformGroup allTransforms = new TransformGroup();
+              Grid canvas = armSideViewPolygon2.Parent as Grid;
+              RotateTransform rotate = new RotateTransform((e - FirstChange) * 135);
+              BetweenAngle = e;
+              rotate.CenterX = canvas.Width / 2 + 50 + TransitionX;
+              rotate.CenterY = canvas.Height / 2 + 20 + TransitionY;
+              allTransforms.Children.Add(new TranslateTransform(TransitionX, TransitionY));
+              allTransforms.Children.Add(rotate);
+
+              armSideViewPolygon2.RenderTransform = allTransforms;
+          }
     }
+
 }
