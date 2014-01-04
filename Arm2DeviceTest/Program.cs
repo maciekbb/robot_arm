8d88d4db32726b1ffeb92436523b926793ce1717
Jakub Krawczyk <pinkikvk@gmail.com>
2014-01-04 21:51:10 +0100

    Change platform target to x86 for all projects

--- Arm2DeviceTest/Program.cs
+++ Arm2DeviceTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Arm2;
 
 namespace Arm2DeviceTest
 {
@@ -9,6 +10,17 @@ namespace Arm2DeviceTest
     {
         static void Main(string[] args)
         {
+            Arm2Device device = new Arm2Device();
+            if(device.Open() == true)
+            {
+                Console.WriteLine("Device found");
+                device.Close();
+            }
+            else
+            {
+                Console.WriteLine("Device not found");
+            }
+            Console.ReadLine();
         }
     }
 }
